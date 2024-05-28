d05d761aacf429dd38a269fa45cc5ee12f1a594d
Mahodi313 <MehdiAli15@outlook.com>
2024-05-28 21:04:00 +0200

    feature/counter-shoppingcart

--- src/components/cart-icon/Cart-icon.component.jsx
+++ src/components/cart-icon/Cart-icon.component.jsx
@@ -7,14 +7,14 @@ import ShoppingIcon from "../../assets/shopping-bag.svg?react";
 import "./Cart-icon.styles.css";
 
 function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <li className="nav-item cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </li>
   );
 }
