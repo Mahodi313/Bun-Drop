154bae96f543027ff2fbf4aa5498b7b4b1004279
Mahodi313 <MehdiAli15@outlook.com>
2024-05-28 17:23:04 +0200

    fix/key-prop-bug

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
 
 import CartItem from "../cart-item/cart-item.component";
@@ -8,22 +10,23 @@ import "./cart-dropdown.styles.css";
 
 function CartDropdown() {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
         {cartItems.length ? (
           cartItems.map((cartItem) => (
-            <>
+            <div key={cartItem.id}>
               <CartItem key={cartItem.id} cartItem={cartItem} />
               <hr />
-            </>
+            </div>
           ))
         ) : (
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <button>GO TO CHECKOUT</button>
+      <button onClick={() => navigate("/checkout")}>Go to checkout</button>
     </div>
   );
 }
